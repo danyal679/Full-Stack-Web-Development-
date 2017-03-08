090f66ba7917261c1e91a7fe35fa1ab70a3362bd
danyal679 <danyalll123@icloud.com>
2017-03-08 17:34:48 +0000

    Add files via upload

---
+++ unit 1/integer/integer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integer
+{
+    class SecondaryClass
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            int answer;
+            //string operand;
+
+            Console.Write("Please enter the first integer: ");
+
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Please enter an operand (+, -, /, *): ");
+
+            //operand = Console.ReadLine();
+
+            Console.Write("Please enter the second integer: ");
+
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            answer = firstNumber * secondNumber;
+
+            Console.WriteLine(firstNumber.ToString() + " " + "*" + " " + secondNumber.ToString() + " = " + answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ unit 1/user input/user input/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // Loop indefinitely
+            { 
+            Console.WriteLine("Enter input:"); // Prompt the user 
+            String userInput = Console.ReadLine(); // Get string from user
+            if (userInput == "exit") // Check string
+            {
+                break;
+            }
+            Console.Write("you typed"); // Report output
+            Console.Write(userInput.Length);
+            Console.WriteLine(" character(s) ");
+            Console.ReadKey();
+            break;
+        } 
+    }
+    }
+}
